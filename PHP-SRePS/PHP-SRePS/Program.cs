6383a2abb2c57556d10c5ef8e5160d087373372f
Jonathon S <102579324@student.swin.edu.au>
2021-05-04 16:41:08 +1000

    SQLite groundwork

--- PHP-SRePS/PHP-SRePS/Program.cs
+++ PHP-SRePS/PHP-SRePS/Program.cs
@@ -1,36 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+//namespace PHP_SRePS
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Product test = new Product(1, "Test", 5);
+//            Product test2 = new Product(2, "Test2", 10);
+//            Product test3 = new Product(3, "Test3", 1);
+//            Product test4 = new Product(4, "Test4", 7);
+//            List<Product> Products = new List<Product>();
+//            List<int> Quantities = new List<int>();
+//            Products.Add(test);
+//            Products.Add(test2);
+//            Products.Add(test3);
+//            Products.Add(test4);
+//            Quantities.Add(4);
+//            Quantities.Add(6);
+//            Quantities.Add(22);
+//            Quantities.Add(8);
+//            Sale testSale = new Sale(1738, 324, Products, Quantities, DateTime.Today);
+//            List<Sale> testSalesList = new List<Sale>();
+//            testSalesList.Add(testSale);
+//            SalesReport testSalesReport = new SalesReport(420, testSalesList, DateTime.Today);
+//            SalesReport readInReport = new SalesReport();
+//            testSalesReport.writeCSV("test");
+//        }
+//    }
+//}
+
 
 namespace PHP_SRePS
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Product test = new Product(1, "Test", 5);
-            Product test2 = new Product(2, "Test2", 10);
-            Product test3 = new Product(3, "Test3", 1);
-            Product test4 = new Product(4, "Test4", 7);
-            List<Product> Products = new List<Product>();
-            List<int> Quantities = new List<int>();
-            Products.Add(test);
-            Products.Add(test2);
-            Products.Add(test3);
-            Products.Add(test4);
-            Quantities.Add(4);
-            Quantities.Add(6);
-            Quantities.Add(22);
-            Quantities.Add(8);
-            Sale testSale = new Sale(1738, 324, Products, Quantities, DateTime.Today);
-            List<Sale> testSalesList = new List<Sale>();
-            testSalesList.Add(testSale);
-            SalesReport testSalesReport = new SalesReport(420, testSalesList, DateTime.Today);
-            SalesReport readInReport = new SalesReport();
-            testSalesReport.writeCSV("test");
-            readInReport.readCSV("test");
-            //readInReport.writeCSV("readCheck");
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            DataTable t = sqlite_conn.GetSchema("Tables");
+            ReadData(sqlite_conn);
+        }
 
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=PHP.db; Version = 3; New = True; Compress = True; ");
+            Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        static void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE SampleTable (Col1 VARCHAR(20), Col2 INT)";
+            string Createsql1 = "CREATE TABLE SampleTable1 (Col1 VARCHAR(20), Col2 INT)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = Createsql1;
+            sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable (Col1, Col2) VALUES('Test Text ', 1); ";
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM sales";
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
         }
     }
 }
--- PHP-SRePS/PHP-SRePS/SalesReport.cs
+++ PHP-SRePS/PHP-SRePS/SalesReport.cs
@@ -134,50 +134,50 @@ namespace PHP_SRePS
                 }
             }
         }
-        public void readCSV(string name)
-        {
-            List<Sale> Sales = new List<Sale>();
-            DateTime control = DateTime.Today;
-            string fullname = name + ".csv";
-            using (var reader = new StreamReader(fullname))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                csv.Read();
-                string idString = csv.GetField(0);
-                int ID = int.Parse(idString);
-                int salesCount = csv.GetRecord<int>();
-                csv.Read();
-                for (int i = 0; i < salesCount; i++)
-                {
-                    int saleId = csv.GetRecord<int>();
-                    csv.Read();
-                    int saleUserId = csv.GetRecord<int>();
-                    csv.Read();
-                    int prodCount = csv.GetRecord<int>();
-                    csv.Read();
-                    List<Product> tempProducts = new List<Product>();
-                    List<int> tempQuantity = new List<int>();
-                    Sale tempSale = new Sale(saleId, saleUserId, tempProducts, tempQuantity, control);
-                    for (int a = 0; a < prodCount; a++)
-                    {
-                        int prodID = csv.GetRecord<int>();
-                        csv.Read();
-                        string prodName = csv.GetRecords<string>();
-                        csv.Read();
-                        string prodPriceString = csv.GetField(0);
-                        decimal prodPrice = decimal.Parse(prodPriceString);
-                        csv.Read();
-                        int prodQ = csv.GetRecord<int>();
-                        csv.Read();
-                        Product tempProd = new Product(prodID, prodName, prodPrice);
-                        tempSale.AddItem(tempProd, prodQ);
-                    }
-                    Sales.Add(tempSale);
-                }
-                _id = ID;
-                _reportDate = control;
-                _sales = Sales;
-            }
-        }
+        //public void readCSV(string name)
+        //{
+        //    List<Sale> Sales = new List<Sale>();
+        //    DateTime control = DateTime.Today;
+        //    string fullname = name + ".csv";
+        //    using (var reader = new StreamReader(fullname))
+        //    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        //    {
+        //        csv.Read();
+        //        string idString = csv.GetField(0);
+        //        int ID = int.Parse(idString);
+        //        int salesCount = csv.GetRecord<int>();
+        //        csv.Read();
+        //        for (int i = 0; i < salesCount; i++)
+        //        {
+        //            int saleId = csv.GetRecord<int>();
+        //            csv.Read();
+        //            int saleUserId = csv.GetRecord<int>();
+        //            csv.Read();
+        //            int prodCount = csv.GetRecord<int>();
+        //            csv.Read();
+        //            List<Product> tempProducts = new List<Product>();
+        //            List<int> tempQuantity = new List<int>();
+        //            Sale tempSale = new Sale(saleId, saleUserId, tempProducts, tempQuantity, control);
+        //            for (int a = 0; a < prodCount; a++)
+        //            {
+        //                int prodID = csv.GetRecord<int>();
+        //                csv.Read();
+        //                string prodName = csv.GetField(0);
+        //                csv.Read();
+        //                string prodPriceString = csv.GetField(0);
+        //                decimal prodPrice = decimal.Parse(prodPriceString);
+        //                csv.Read();
+        //                int prodQ = csv.GetRecord<int>();
+        //                csv.Read();
+        //                Product tempProd = new Product(prodID, prodName, prodPrice);
+        //                tempSale.AddItem(tempProd, prodQ);
+        //            }
+        //            Sales.Add(tempSale);
+        //        }
+        //        _id = ID;
+        //        _reportDate = control;
+        //        _sales = Sales;
+        //    }
+        //}
     }
 }
